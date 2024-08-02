4367a25ebebf67d92522e52a0f092f5cad0ba070
Anmol Bhardwaj <anmolbhardwajinv@gmail.com>
2024-08-02 08:09:45 +0530

    practice

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st 
+import time  # to show progress 
+
 
 
 
@@ -61,7 +63,65 @@ add_slider = st.sidebar.slider(
     0.0, 100.0, (25.0, 75.0)
 )
 
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
 st.write("Hello World !")
+
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
+
+# Caching in streamlit 
+
+
+@st.cache_data
+def long_running_function(param1, param2):
+    return …
+
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
+
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
+
+st.header("Choose a datapoint color")
+color = st.color_picker("Color", "#FF0000")
+st.divider()
+st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
+
+
+
+conn = st.connection("my_database")
+df = conn.query("select * from my_table")
+st.dataframe(df)
+
 
 """
 # My first app
