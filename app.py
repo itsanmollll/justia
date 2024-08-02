cb7a09555cb8597e53e49b0d136264d5cbb3d7aa
Anmol Bhardwaj <anmolbhardwajinv@gmail.com>
2024-08-02 07:16:00 +0530

    First Commmit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+import pandas as pd
+
+st.write("Hello World !")
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
